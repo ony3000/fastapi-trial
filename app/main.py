ed3a0a32e8822c79aa20616c2c163daa0dae6335
Hyeonjong <nianelo4@gmail.com>
2022-04-04 22:44:48 +0900

    Add get api with enum parameter

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Optional
 
 from fastapi import FastAPI
@@ -39,3 +40,20 @@ def update_item(
         "item_name": item.name,
         "item_id": item_id,
     }
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
