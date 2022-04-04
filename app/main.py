3edff352fa7107a0ecf2dfa023346f293e80f074
Hyeonjong <nianelo4@gmail.com>
2022-04-04 21:41:02 +0900

    Add some linebreaks for better readability

--- app/main.py
+++ app/main.py
@@ -14,14 +14,28 @@ class Item(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {
+        "Hello": "World",
+    }
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(
+    item_id: int,
+    q: Optional[str] = None,
+):
+    return {
+        "item_id": item_id,
+        "q": q,
+    }
 
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+def update_item(
+    item_id: int,
+    item: Item,
+):
+    return {
+        "item_name": item.name,
+        "item_id": item_id,
+    }
